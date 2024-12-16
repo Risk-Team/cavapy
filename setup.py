606266a20e959c323dac193ff0b1d1d15e785405
Riccardo Soldan <riccardosoldan@hotmail.it>
2024-12-16 13:46:08 +0100

    updated to use conda

--- setup.py
+++ setup.py
@@ -1,22 +1,20 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name="cava_data",
-    version="0.1.0",
-    py_modules=["climate"],
+    name='cavapy',
+    version='0.1.0',
+    packages=find_packages(),  # Automatically discover all packages in the current directory
     install_requires=[
-        "xclim==0.53.2",
-        "netCDF4=1.7.2",
-        "geopandas==0.14.4"
+        'xclim>=0.53.2',
+        'netcdf4==1.7.2',
+        'geopandas==0.14.4',
     ],
-    description="A python library to facilitate access to CAVA data",
-    author="Riccardo Soldan, Oleh Lokshyn",
-    author_email="riccardosoldan@hotmail.it",
-    url="https://github.com/Risk-Team/cava_data/tree/main",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            'climate = climate:get_climate_data',  # If you have a `main` function in climate.py, this will make it callable
+        ],
+    },
+    package_data={
+        '': ['climate.py'],  # Include climate.py in the package
+    }
 )
